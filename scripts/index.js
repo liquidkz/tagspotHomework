cac96cc1a707657290130fadcd18afba58f57389
AlibekZhunusov <alibekzhu@gmail.com>
2025-06-02 10:34:26 +0500

    feature(): add cards and delete cards funct

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+
+function createCard(cardData) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+    cardsList.append(cardElement);
+});
 
 // @todo: DOM узлы
 
